c0002504d803db897be445c2c4b7d1ad0ab5e34c
obirijahtochukwu <tobirijah@gmail.com>
2024-12-16 18:07:07 +0100

    fixed deploy bug

--- src/middleware/corsConfig.js
+++ src/middleware/corsConfig.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const corsConfig = cors({
   origin: "http://localhost:3000",
   methods: "GET, POST, PATCH, DELETE",
-  allowedHeaders: ["Content-type", "Authorization"],
   credentials: true,
 });
 
