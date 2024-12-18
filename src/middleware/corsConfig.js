68b77c3566307f54aa063271462debabf6bc4497
obirijahtochukwu <tobirijah@gmail.com>
2024-12-18 13:24:44 +0100

    fixed build bug

--- src/middleware/corsConfig.js
+++ src/middleware/corsConfig.js
@@ -4,7 +4,6 @@ const corsConfig = cors({
   origin: "http://localhost:3000",
   methods: "GET, POST, PATCH, DELETE",
   credentials: true,
-  optionSuccessStatus: 200,
 });
 
 module.exports = corsConfig;
