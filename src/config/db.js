b9493361dd9bb1b5639128a455979ff9b1328358
obirijahtochukwu <tobirijah@gmail.com>
2025-01-06 23:43:13 +0100

    moved game logic to backend

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const URL = "mongodb+srv://obj:obj123,.@cluster0.nfbux.mongodb.net/";
+const URL =
+  "mongodb://localhost:27017/" ||
+  "mongodb+srv://obj:obj123,.@cluster0.nfbux.mongodb.net/";
 
 const connectDB = async () => {
   try {
